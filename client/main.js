87cea00cb0c7f2723ecb4d77bacbe60fad1c6b21
Loek van Gent <loek@1procentclub.nl>
2014-06-27 12:27:03 +0200

    Change style & add leapmotion

--- client/main.js
+++ client/main.js
@@ -1,59 +1,72 @@
 App = {
-  defaults: {
-    skip: 7,
-  }
+    defaults: {
+        skip: 7,
+    }
 }
 
 interval = null;
 
 Meteor.startup(function () {
-  Session.set('limit', App.defaults.skip);
-  Session.setDefault('projectSlugs', []);
+    Session.set('limit', App.defaults.skip);
+    Session.setDefault('projectSlugs', []);
 
-  Deps.autorun(function (computation) {
-    Meteor.subscribe('messages', {limit: Session.get('limit')}, function (response) {
-      if (Session.get('limit') > Messages.find().count())
-        Session.set('hideLoadMore', true);
-      else
-        Session.set('hideLoadMore', false);
+    LeapManager.init({
+        maxCursors:1,
+        enableHoverTap: true,
+        enablePressDown: true,
+        enableTouchScrolling: true,
+        enableScrollbarScrolling: true,
+        simulateWithMouse:false,
+        mouseCursorConfig: {
+            easing:.2
+        },
+        greedySelector: "body"
     });
-  });
 
-  Deps.autorun(function (computation) {
-    Session.set('campaignSlugs', Messages.find().map( function (message) {
-      if (message.raw && message.raw.project)
-        return message.raw.project.id;
-    }));
+    Deps.autorun(function (computation) {
+        Meteor.subscribe('messages', {limit: Session.get('limit')}, function (response) {
+            if (Session.get('limit') > Messages.find().count())
+                Session.set('hideLoadMore', true);
+            else
+                Session.set('hideLoadMore', false);
+        });
+    });
 
-    Session.set('campaignSlug', Session.get('campaignSlugs')[0]);
-  });
+    Deps.autorun(function (computation) {
+        Session.set('campaignSlugs', Messages.find().map( function (message) {
+            if (message.raw && message.raw.project)
+                return message.raw.project.id;
+        }));
 
-  Deps.autorun(function (computation) {
-    var message = Messages.findOne({'raw.project.id': Session.get('campaignSlug')});
-    if (message && message.raw && message.raw.project)
-      Session.set('campaign', message.raw.project);
-  });
+        Session.set('campaignSlug', Session.get('campaignSlugs')[0]);
+    });
 
-  Deps.autorun(function (computation) {
-    // Clear old timer
-    if (interval) Meteor.clearInterval(interval);
+    Deps.autorun(function (computation) {
+        var message = Messages.findOne({'raw.project.id': Session.get('campaignSlug')});
+        if (message && message.raw && message.raw.project)
+            Session.set('campaign', message.raw.project);
+    });
 
-    var slugs = Session.get('campaignSlugs'),
-        index = 0;
+    Deps.autorun(function (computation) {
+        // Clear old timer
+        if (interval) Meteor.clearInterval(interval);
 
-    // No timer needed if there are no slugs
-    if (!slugs.length) return;
-        
-    var timeLeft = function () {
-      Session.set('campaignSlug', slugs[index]);
-      if (index < slugs.length - 1) {
-        index++;
-      } else {
-        index = 0;
-      }
-    }
+        var slugs = Session.get('campaignSlugs'),
+            index = 0;
 
-    // Set timer for 10 seconds
-    interval = Meteor.setInterval(timeLeft, 10*1000);
-  });
+        // No timer needed if there are no slugs
+        if (!slugs.length) return;
+
+        var timeLeft = function () {
+            Session.set('campaignSlug', slugs[index]);
+            if (index < slugs.length - 1) {
+                index++;
+            } else {
+                index = 0;
+            }
+        }
+
+        // Set timer for 10 seconds
+        interval = Meteor.setInterval(timeLeft, 10*1000);
+    });
 });
--- client/views/message.js
+++ client/views/message.js
@@ -1,18 +1,18 @@
 Template.message.helpers({
-	formattedDate: function () {
-		return moment(this.created).format("dddd, MMMM Do YYYY, h:mm a");
-	},
-  hideMessage: function () {
-    return this.raw && this.raw.project && this.raw.project.id == Session.get('campaign').id;
-  },
-  amount: function () {
-    return this.raw && this.raw.amount;
-  }
+    formattedDate: function () {
+        return moment(this.created).fromNow();
+    },
+    selectedMessage: function () {
+        return this.raw && this.raw.project && this.raw.project.id == Session.get('campaign').id;
+    },
+    amount: function () {
+        return this.raw && Math.round(this.raw.amount);
+    }
 });
 
 Template.message.image = function() {
     if (!this.raw || !this.raw.user)
-      return ''
+        return ''
 
     var raw = this.raw,
         user = raw.user;
